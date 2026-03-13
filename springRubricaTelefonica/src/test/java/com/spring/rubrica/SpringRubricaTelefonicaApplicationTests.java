c0d8a4d3cc8a8d0ca620312327c7034d436735b6
pastaepiselli <rossilorenzo371@gmail.com>
2026-03-13 17:17:59 +0100

    Spring Rubirca da fare :P

---
+++ springRubricaTelefonica/src/test/java/com/spring/rubrica/SpringRubricaTelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rubrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRubricaTelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
